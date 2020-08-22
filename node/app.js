230077b2e370c63ea71579d997eb033e2899df69
elianiva <dicha.arkana03@gmail.com>
2020-08-22 20:03:06 +0700

    added some functions

--- node/app.js
+++ node/app.js
@@ -1,19 +1,16 @@
-const { say } = require('../pkg/ssvm_nodejs_starter_lib.js');
+const { generate_colors } = require("../pkg/ssvm_nodejs_starter_lib.js")
 
-const http = require('http');
-const url = require('url');
-const hostname = '0.0.0.0';
-const port = 3000;
+const http = require("http")
+const url = require("url")
+const hostname = "0.0.0.0"
+const port = 3000
 
 const server = http.createServer((req, res) => {
-  const queryObject = url.parse(req.url,true).query;
-  if (!queryObject['name']) {
-    res.end(`Please use command curl http://${hostname}:${port}/?name=MyName \n`);
-  } else {
-    res.end(say(queryObject['name']) + '\n');
-  }
-});
+  const queryObject = url.parse(req.url, true).query
+  const { mode, max } = queryObject
+  res.end(generate_colors(parseInt(max), mode) + "\n")
+})
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,70 @@
+extern crate rand;
 use wasm_bindgen::prelude::*;
+use rand::Rng;
+
+pub fn generate_rgb(max: i32) -> String {
+    let mut colors: Vec<String> = Vec::new();
+    let mut rgb: Vec<String> = Vec::new();
+
+    for _ in 0..max {
+        for _ in 0..3 {
+            let new: i32 = rand::thread_rng().gen_range(0, 255);
+            rgb.push(new.to_string());
+        }
+        colors.push(format!("rgb({})", rgb.join(",")));
+        rgb.clear();
+    }
+
+    return colors.join(" ");
+}
+
+pub fn generate_hsl(max: i32) -> String {
+    let mut colors: Vec<String> = Vec::new();
+    let mut hsl: Vec<String> = Vec::new();
+
+    for _ in 0..max {
+        for _ in 0..3 {
+            let new: i32 = rand::thread_rng().gen_range(0, 100);
+            hsl.push(new.to_string());
+        }
+        colors.push(format!("hsl({})", hsl.join(",")));
+        hsl.clear();
+    }
+
+    return colors.join(" ");
+}
+
+pub fn generate_hex(max: i32) -> String {
+    let mut colors: Vec<String> = Vec::new();
+    let mut hex: Vec<String> = Vec::new();
+
+    for _ in 0..max {
+        for _ in 0..3 {
+            let new: i32 = rand::thread_rng().gen_range(0, 255);
+
+            if new < 10 {
+                hex.push(format!("0{}", new));
+            } else {
+                hex.push(format!("{:X}", new));
+            }
+        }
+        colors.push(format!("#{}", hex.join("")));
+        hex.clear();
+    }
+
+    return colors.join(" ");
+}
+
+pub fn print_error() -> String {
+    return "Wrong request".to_string();
+}
 
 #[wasm_bindgen]
-pub fn say(s: &str) -> String {
-  println!("The Rust function say() received {}", s);
-  let r = String::from("hello ");
-  return r + s;
+pub fn generate_colors(max: i32, mode: &str) -> String {
+    match mode {
+        "rgb" => generate_rgb(max),
+        "hex" => generate_hex(max),
+        "hsl" => generate_hsl(max),
+        _ => print_error()
+    }
 }
